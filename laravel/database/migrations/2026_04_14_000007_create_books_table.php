f40307d30f6e5c32204e9d48c35b60780e7e160d
Sofie <sov.002@edu.zealand.dk>
2026-04-14 17:41:24 +0200

    Migrationsss wup wup!

--- laravel/database/migrations/2026_04_14_000007_create_books_table.php
+++ laravel/database/migrations/2026_04_14_000007_create_books_table.php
@@ -14,6 +14,18 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->date('publishing_date');
+            $table->string('description');
+            $table->string('image');
+            $table-> decimal('price', 8, 2);
+            $table->foreignId('genre_id')->constrained();
+            //Constrained er reference genre(id)=> genre.id, forsøger
+            // man at slette bog der har tilknytning til genre_id
+            // så ville den restrict.
+
+
+
         });
     }
 
