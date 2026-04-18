80cc87feba60418f82958352ea357a43f977e1f7
marta <m.nowak033@gmail.com>
2026-04-18 10:54:38 +0200

    Update Order model to use a relationship with book

--- laravel/app/Models/Order.php
+++ laravel/app/Models/Order.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+    public function book(): BelongsToMany {
+        return $this->belongsToMany(Book::class);
     }
 }
