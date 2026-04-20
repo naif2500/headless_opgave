0d9ab1473b634657fb5a68de117be317fb33a633
Sofie <sov.002@edu.zealand.dk>
2026-04-20 12:34:50 +0200

    add files+changes in AuthController

--- laravel/app/Http/Controllers/Api/AuthController.php
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -1,49 +1,24 @@
 <?php
-
-namespace App\Http\Controllers\Api;
-
-use App\Http\Controllers\Controller;
+use\App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
+class AuthController extends Controller{
+public function login(Request $request){
+    $retuest->validate([
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+}
+$user = App\Models\User::where('email', $request->email)->first();
+if(!$user )
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+    throw ValidationException::withMessage([
+        'email' => ['The provided credentials are incorrect.']
+    ]);
+    if(!Hash::check($request->password, $user->password)){
+        throw ValidationException::withMessage([
+            'email' => ['The provided credentials are incorrect.']
+        ]);
     }
 }
