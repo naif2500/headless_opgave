f929e56b5361a64fa2a70c51abe31b1c71c9bb37
Sofie <sov.002@edu.zealand.dk>
2026-04-22 20:46:46 +0200

    tilføje ->statefulApi() paa linje 15 i boostrap/cahce/app.php

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -1,7 +1,21 @@
+<?php
+
 return [
+
     'paths' => ['api/*', 'login', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => ['http://localhost:3000'],
 
-    'allowed_credentials' => true,
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
 ];
