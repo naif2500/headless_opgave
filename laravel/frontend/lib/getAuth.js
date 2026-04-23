83b305933b7abab4e0dc151cba9d8b0094d0f6e4
marta <m.nowak033@gmail.com>
2026-04-23 19:27:19 +0200

    Add a function to retrieve a cookie in the FE

---
+++ laravel/frontend/lib/getAuth.js
@@ -0,0 +1,11 @@
+export function getAuth() {
+    if (typeof window === "undefined") return false;
+
+    const cookies = document.cookie.split("; "); //splitter min cokkies op i et array
+    const sessionCookie = cookies.find(
+        (row) =>
+            row.startsWith("laravel_session=") || row.startsWith("XSRF-TOKEN="),
+    );
+
+    return !!sessionCookie;
+}
